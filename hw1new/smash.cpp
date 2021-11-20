50813c4beb16dd14d62c62c14fcc8a8fc7479e8a
nir.taly <nir.taly@gmail.com>
2021-11-20 15:24:27 +0200

    start1

--- hw1new/smash.cpp
+++ hw1new/smash.cpp
@@ -7,9 +7,9 @@
 
 
 int main(int argc, char* argv[]) {
-    if(signal(SIGTSTP , ctrlZHandler)==SIG_ERR) {
-        perror("smash error: failed to set ctrl-Z handler");
-    }
+    // if(signal(SIGTSTP , ctrlZHandler)==SIG_ERR) {
+    //     perror("smash error: failed to set ctrl-Z handler");
+    // }
     // if(signal(SIGINT , ctrlCHandler)==SIG_ERR) {
     //     perror("smash error: failed to set ctrl-C handler");
     // }
@@ -17,16 +17,10 @@ int main(int argc, char* argv[]) {
     //TODO: setup sig alarm handler
 
     SmallShell& smash = SmallShell::getInstance();
-    int i = 0;
     while(true) {
         std::cout << smash.getPrompt() <<"> ";
         std::string cmd_line;        
-        //my tests
-        if(i > 3){
-            break;
-        }
-        i++;
-        //end of tests
+
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
     }
